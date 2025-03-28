32cbc675421143cfe46eb5d8a99477ae52471071
lhakiim <luqmanulhakim2021@gmail.com>
2025-03-28 14:06:51 +0700

    Implement add function in Subscriber repository.

--- src/repository/subscriber.rs
+++ src/repository/subscriber.rs
@@ -11,4 +11,14 @@ pub struct SubscribeRepository;
 
 impl SubscribeRepository {
     
+    pub fn add(product_type: &str, subscriber: Subscriber) -> Subscriber {
+        let subscriber_value = subscriber.clone();
+        if SUBSCRIBERS.get(product_type).is_none() {
+            SUBSCRIBERS.insert(String::from(product_type), DashMap::new());
+        }
+
+        SUBSCRIBERS.get(product_type).unwrap()
+            .insert(subscriber_value.url.clone(), subscriber_value);
+        return subscriber;
+    }    
 }
